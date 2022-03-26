f94d3e8113d2f343c0ba3f0f89540b66ee1b1772
anunay <i.anunay@gmail.com>
2022-03-27 04:39:25 +0530

    init

---
+++ inventory/consumer.py
@@ -0,0 +1,33 @@
+from main import redis, Product
+import time
+
+key = 'order_completed'
+group = 'inventory-group'
+
+try:
+    redis.xgroup_create(key, group)
+except:
+    print('Group already exists!')
+
+while True:
+    try:
+        results = redis.xreadgroup(group, key, {key: '>'}, None)
+        print(results)
+
+        if results != []:
+            for result in results:
+                obj = result[1][0][1]
+                try:
+                    product = Product.get(obj['product_id'])
+                    print('abcd')
+                    if product.quantity < int(obj['quantity']) or product.quantity - int(obj['quantity']) < 0:
+                        raise Exception
+                    product.quantity = product.quantity - int(obj['quantity'])
+                    print(product.quantity, int(obj['quantity']))
+                    product.save()
+                except:
+                    redis.xadd('refund_order', obj, '*')
+
+    except Exception as e:
+        print(str(e))
+    time.sleep(1)
